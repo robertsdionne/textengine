22a72cb05a6a102b6a4964db5812d447ab3a1c53
Robert Scott Dionne <robertsdionne@gmail.com>
2013-09-18 13:32:50 -0400

    Create constants for the window properties.

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,14 @@
 #include "glfwapplication.h"
 #include "textenginerenderer.h"
 
+constexpr int kWindowHeight = 800;
+constexpr int kWindowWidth = 1280;
+constexpr const char *kWindowTitle = "textengine";
+
 int main(int argument_count, char *arguments[]) {
   textengine::TextEngineRenderer renderer;
   textengine::GlfwApplication application(argument_count, arguments,
-                                          1280, 800, "textengine", &renderer);
+                                          kWindowWidth, kWindowHeight, kWindowTitle, &renderer);
   application.Run();
   return 0;
 }
