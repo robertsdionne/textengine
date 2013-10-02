d14717b60d3cbe5a9d556f3ebe311831c3423a17
Robert Scott Dionne <robertsdionne@gmail.com>
2013-10-01 20:29:43 -0400

    Use a map to cache indices for saving meshes.

--- source/meshserializer.cpp
+++ source/meshserializer.cpp
@@ -9,76 +9,64 @@ namespace textengine {
   void MeshSerializer::WriteMesh(const std::string &filename, const Mesh &mesh) const {
     std::ofstream out(filename);
     CHECK_STATE(!out.fail());
+    VertexIndexMap vertex_indices = {{nullptr, -1}};
+    for (auto i = 0; i < mesh.get_vertices().size(); ++i) {
+      vertex_indices[mesh.get_vertices()[i].get()] = i;
+    }
+    HalfEdgeIndexMap half_edge_indices = {{nullptr, -1}};
+    for (auto i = 0; i < mesh.get_half_edges().size(); ++i) {
+      half_edge_indices[mesh.get_half_edges()[i].get()] = i;
+    }
+    FaceIndexMap face_indices = {{nullptr, -1}};
+    for (auto i = 0; i < mesh.get_faces().size(); ++i) {
+      face_indices[mesh.get_faces()[i].get()] = i;
+    }
     picojson::array vertices;
     for (auto &vertex : mesh.get_vertices()) {
-      vertices.push_back(WriteVertex(mesh, vertex.get()));
+      vertices.push_back(WriteVertex(half_edge_indices, vertex.get()));
     }
     picojson::array half_edges;
     for (auto &half_edge : mesh.get_half_edges()) {
-      half_edges.push_back(WriteHalfEdge(mesh, half_edge.get()));
+      half_edges.push_back(WriteHalfEdge(face_indices, half_edge_indices,
+                                         vertex_indices, half_edge.get()));
     }
     picojson::array faces;
     for (auto &face : mesh.get_faces()) {
-      faces.push_back(WriteFace(mesh, face.get()));
+      faces.push_back(WriteFace(half_edge_indices, face.get()));
     }
     picojson::object object;
     object["vertices"] = picojson::value(vertices);
     object["half_edges"] = picojson::value(half_edges);
     object["faces"] = picojson::value(faces);
-    out << picojson::value(object);
+    out << picojson::value(object) << std::endl;
     out.close();
   }
 
-  double MeshSerializer::FindVertexIndex(const textengine::Mesh &mesh,
-                                    const Mesh::Vertex *vertex) const {
-    for (auto i = 0; i < mesh.get_vertices().size(); ++i) {
-      if (vertex == mesh.get_vertices()[i].get()) {
-        return i;
-      }
-    }
-    return -1.0;
-  }
-
-  double MeshSerializer::FindHalfEdgeIndex(const textengine::Mesh &mesh,
-                                    const Mesh::HalfEdge *half_edge) const {
-    for (auto i = 0; i < mesh.get_half_edges().size(); ++i) {
-      if (half_edge == mesh.get_half_edges()[i].get()) {
-        return i;
-      }
-    }
-    return -1.0;
-  }
-
-  double MeshSerializer::FindFaceIndex(const textengine::Mesh &mesh, const Mesh::Face *face) const {
-    for (auto i = 0; i < mesh.get_faces().size(); ++i) {
-      if (face == mesh.get_faces()[i].get()) {
-        return i;
-      }
-    }
-    return -1.0;
-  }
-
-  picojson::value MeshSerializer::WriteVertex(const Mesh &mesh, const Mesh::Vertex *vertex) const {
+  picojson::value MeshSerializer::WriteVertex(const HalfEdgeIndexMap &half_edge_indices,
+                                              const Mesh::Vertex *vertex) const {
     picojson::object object;
     object["position"] = WriteVec2(vertex->position);
-    object["vertex_edge"] = picojson::value(FindHalfEdgeIndex(mesh, vertex->vertex_edge));
+    object["vertex_edge"] = picojson::value(half_edge_indices.at(vertex->vertex_edge));
     return picojson::value(object);
   }
 
-  picojson::value MeshSerializer::WriteHalfEdge(const Mesh &mesh,
+  picojson::value MeshSerializer::WriteHalfEdge(const FaceIndexMap &face_indices,
+                                                const HalfEdgeIndexMap &half_edge_indices,
+                                                const VertexIndexMap &vertex_indices,
                                                 const Mesh::HalfEdge *half_edge) const {
     picojson::object object;
-    object["face"] = picojson::value(FindFaceIndex(mesh, half_edge->face));
-    object["next"] = picojson::value(FindHalfEdgeIndex(mesh, half_edge->next));
-    object["opposite"] = picojson::value(FindHalfEdgeIndex(mesh, half_edge->opposite));
-    object["previous"] = picojson::value(FindHalfEdgeIndex(mesh, half_edge->previous));
-    object["start"] = picojson::value(FindVertexIndex(mesh, half_edge->start));
+    object["face"] = picojson::value(face_indices.at(half_edge->face));
+    object["next"] = picojson::value(half_edge_indices.at(half_edge->next));
+    object["opposite"] = picojson::value(half_edge_indices.at(half_edge->opposite));
+    object["previous"] = picojson::value(half_edge_indices.at(half_edge->previous));
+    object["start"] = picojson::value(vertex_indices.at(half_edge->start));
     return picojson::value(object);
   }
 
-  picojson::value MeshSerializer::WriteFace(const Mesh &mesh, const Mesh::Face *face) const {
+  picojson::value MeshSerializer::WriteFace(const HalfEdgeIndexMap &half_edge_indices,
+                                            const Mesh::Face *face) const {
     picojson::object object;
-    object["face_edge"] = picojson::value(FindHalfEdgeIndex(mesh, face->face_edge));
+    object["face_edge"] = picojson::value(half_edge_indices.at(face->face_edge));
     return picojson::value(object);
   }
 
--- source/meshserializer.h
+++ source/meshserializer.h
@@ -3,6 +3,7 @@
 
 #include <picojson.h>
 #include <string>
+#include <unordered_map>
 
 namespace textengine {
 
@@ -17,17 +18,20 @@ namespace textengine {
     void WriteMesh(const std::string &filename, const Mesh &mesh) const;
 
   private:
-    double FindVertexIndex(const Mesh &mesh, const Mesh::Vertex *vertex) const;
+    typedef std::unordered_map<Mesh::Face *, double> FaceIndexMap;
+    typedef std::unordered_map<Mesh::HalfEdge *, double> HalfEdgeIndexMap;
+    typedef std::unordered_map<Mesh::Vertex *, double> VertexIndexMap;
 
-    double FindHalfEdgeIndex(const Mesh &mesh, const Mesh::HalfEdge *half_edge) const;
+    picojson::value WriteVertex(const HalfEdgeIndexMap &half_edge_indices,
+                                const Mesh::Vertex *vertex) const;
 
-    double FindFaceIndex(const Mesh &mesh, const Mesh::Face *face) const;
+    picojson::value WriteHalfEdge(const FaceIndexMap &face_indices,
+                                  const HalfEdgeIndexMap &half_edge_indices,
+                                  const VertexIndexMap &vertex_indices,
+                                  const Mesh::HalfEdge *half_edge) const;
 
-    picojson::value WriteVertex(const Mesh &mesh, const Mesh::Vertex *vertex) const;
-
-    picojson::value WriteHalfEdge(const Mesh &mesh, const Mesh::HalfEdge *half_edge) const;
-
-    picojson::value WriteFace(const Mesh &mesh, const Mesh::Face *face) const;
+    picojson::value WriteFace(const HalfEdgeIndexMap &half_edge_indices,
+                              const Mesh::Face *face) const;
 
     picojson::value WriteVec2(glm::vec2 vector) const;
   };
