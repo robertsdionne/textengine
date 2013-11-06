5153c5ef852060091984682c0bd08a00f27acdb4
Robert Scott Dionne <robertsdionne@gmail.com>
2013-11-06 17:22:34 -0500

    Cleanup.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,3 @@
-#include <fstream>
-#include <glm/glm.hpp>
-#include <random>
 #include <string>
 
 #include "commandparser.h"
@@ -25,20 +22,6 @@ constexpr int kWindowHeight = 800;
 constexpr int kWindowWidth = 1280;
 constexpr const char *kWindowTitle = u8"textengine";
 
-std::random_device generator;
-std::uniform_real_distribution<float> my_random;
-
-void PutItemInRoom(const std::string &item, const std::string &room, textengine::Mesh &mesh, textengine::GameState &state) {
-  for (auto &face : mesh.get_faces()) {
-    if (face->room_info && room == face->room_info->name) {
-      state.items.push_back({
-        face->centroid(), item, glm::vec4(my_random(generator), my_random(generator), my_random(generator), 1)
-      });
-      return;
-    }
-  }
-}
-
 int main(int argument_count, char *arguments[]) {
   const std::string filename = argument_count > 1 ? arguments[1] : "../resource/output.json";
   textengine::Keyboard keyboard;
@@ -53,18 +36,6 @@ int main(int argument_count, char *arguments[]) {
   textengine::CommandTokenizer tokenizer;
   textengine::CommandParser parser{tokenizer, mesh, reply_queue};
   textengine::GameState initial_state;
-  PutItemInRoom("book", "RoomA", mesh, initial_state);
-  PutItemInRoom("teacup", "RoomB", mesh, initial_state);
-  PutItemInRoom("bread", "RoomC", mesh, initial_state);
-  PutItemInRoom("lamp", "RoomD", mesh, initial_state);
-  PutItemInRoom("tire", "RoomE", mesh, initial_state);
-  PutItemInRoom("crowbar", "RoomF", mesh, initial_state);
-  PutItemInRoom("wallet", "RoomG", mesh, initial_state);
-  PutItemInRoom("teapot", "RoomH", mesh, initial_state);
-  PutItemInRoom("coin", "RoomI", mesh, initial_state);
-  PutItemInRoom("pencil", "RoomJ", mesh, initial_state);
-  PutItemInRoom("turtle", "RoomK", mesh, initial_state);
-  PutItemInRoom("note", "RoomL", mesh, initial_state);
   textengine::Log playtest_log{kPlaytestLog};
   textengine::Updater updater{command_queue, reply_queue,
       playtest_log, parser, joystick, mesh, initial_state};
