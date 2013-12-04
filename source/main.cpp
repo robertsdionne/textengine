0f6b79efe476695963a8532d0e600ca90f58b0b4
Robert Scott Dionne <robertsdionne@gmail.com>
2013-12-04 16:36:26 -0500

    Rename the scene file {messages,scene}.json.

--- source/main.cpp
+++ source/main.cpp
@@ -24,7 +24,7 @@ constexpr int kWindowWidth = 1280;
 constexpr const char *kWindowTitle = u8"textengine";
 
 int main(int argument_count, char *arguments[]) {
-  const std::string filename = argument_count > 1 ? arguments[1] : "../resource/subway.json";
+  const std::string filename = argument_count > 1 ? arguments[1] : "../resource/scene.json";
   textengine::Joystick joystick(GLFW_JOYSTICK_1);
   textengine::Keyboard keyboard;
   textengine::Mouse mouse;
@@ -32,7 +32,7 @@ int main(int argument_count, char *arguments[]) {
   textengine::SynchronizedQueue command_queue, reply_queue;
   textengine::Scene scene;
   textengine::SceneLoader scene_loader;
-  scene = scene_loader.ReadScene("../resource/messages.json");
+  scene = scene_loader.ReadScene(filename);
   textengine::CommandTokenizer tokenizer;
   textengine::GameState initial_state{std::vector<std::unique_ptr<std::vector<glm::vec2>>>()};
   textengine::Log playtest_log{kPlaytestLog};
