27fd55522f6f52f5fb078d350b5f7a5559274375
Robert Dionne <robertsdionne@gmail.com>
2014-02-16 20:28:47 -0500

    Make it clear we are using python.

--- source/sceneserializer.cpp
+++ source/sceneserializer.cpp
@@ -25,7 +25,7 @@ namespace textengine {
     object["objects"] = picojson::value(objects);
     out << picojson::value(object) << std::endl;
     out.close();
-    std::system("./../resource/scripts/pretty_print_json.py ../resource/scenes/field.json");
+    std::system("python ../resource/scripts/pretty_print_json.py ../resource/scenes/field.json");
   }
   
   picojson::value SceneSerializer::WriteAxisAlignedBoundingBox(AxisAlignedBoundingBox aabb) const {
